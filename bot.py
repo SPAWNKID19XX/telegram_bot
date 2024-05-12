d2bc7f88c65e2640aa0ec5b5bf4bf202791b961c
spawnkid19xx <borisisac@gmail.com>
2024-05-12 10:56:22 +0100

    [ADD] test bot

--- bot.py
+++ bot.py
@@ -1,4 +1,29 @@
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from secret_settings import *
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 TOKEN = TOKEN
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Я и есть БОТ парень, я и есть БОТ!!!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
+    application.run_polling()
